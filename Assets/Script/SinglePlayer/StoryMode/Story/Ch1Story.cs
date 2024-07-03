d44ea829da2c4cbff632f60195568381bae36abf
meokbo00 <meokbo00@naver.com>
2024-07-03 18:00:34 +0900

    123

--- Assets/Script/SinglePlayer/StoryMode/Story/Ch1Story.cs
+++ Assets/Script/SinglePlayer/StoryMode/Story/Ch1Story.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Ch1Story : MonoBehaviour
@@ -8,7 +10,10 @@ public class Ch1Story : MonoBehaviour
     public GameObject Clearhere;
     private ShowText showText;
     private StageGameManager stageGameManager;
-
+    public Camera mainCamera;
+    public Image FadeIn;
+    public GameObject Stage;
+    public GameObject RemainTime;
     void Start()
     {
         stageGameManager = FindObjectOfType<StageGameManager>();
@@ -28,6 +33,11 @@ public class Ch1Story : MonoBehaviour
         {
             textManager.GiveMeTextId(2);
         }
+        if (stageGameManager.StageClearID == 6)
+        {
+            Destroy(Stage);
+            textManager.GiveMeTextId(3);
+        }
     }
 
     void Update()
@@ -36,14 +46,34 @@ public class Ch1Story : MonoBehaviour
 
         if (showText != null && stageGameManager.StageClearID == 1)
         {
-            // logTextIndex ∞™¿Ã 25¿œ ∂ß ∑Œ±◊ √‚∑¬
-            if (showText.logTextIndex == 26 || showText.logTextIndex == 27 || showText.logTextIndex == 28)
+            if (showText.logTextIndex >= 26 && showText.logTextIndex <= 28)
             {
                 Clearhere.gameObject.SetActive(true);
             }
-            if(showText.logTextIndex >= 27)
+        }
+
+        if (showText != null && stageGameManager.StageClearID == 6)
+        {
+            if (showText.logTextIndex == 4)
             {
-                Clearhere.gameObject.SetActive(false);
+                StartCoroutine(IncreaseCameraSize(mainCamera, 112, 5.5f));
+            }
+            if (showText.logTextIndex == 8)
+            {
+                ContinuousRandomMovement[] randomMovements = FindObjectsOfType<ContinuousRandomMovement>();
+                foreach (ContinuousRandomMovement randomMovement in randomMovements)
+                {
+                    randomMovement.enabled = false;
+                }
+            }
+            if (showText.logTextIndex == 21)
+            {
+                ContinuousRandomMovement[] randomMovements = FindObjectsOfType<ContinuousRandomMovement>();
+                foreach (ContinuousRandomMovement randomMovement in randomMovements)
+                {
+                    randomMovement.enabled = true;
+                }
+                StartCoroutine(HandleCameraAndFadeIn(mainCamera, 15, 7f));
             }
         }
     }
@@ -52,7 +82,47 @@ public class Ch1Story : MonoBehaviour
     {
         if (chatId == 1)
         {
-            Debug.Log("ID 1¿« √§∆√¿Ã øœ∑·µ«æ˙Ω¿¥œ¥Ÿ.");
+            Debug.Log("ID 1 Ï±ÑÌåÖÏù¥ ÏôÑÎ£åÎêòÏóàÏäµÎãàÎã§.");
         }
+    }
+
+    IEnumerator IncreaseCameraSize(Camera camera, float targetSize, float duration)
+    {
+        float startSize = camera.orthographicSize;
+        float timeElapsed = 0f;
+
+        while (timeElapsed < duration)
+        {
+            camera.orthographicSize = Mathf.Lerp(startSize, targetSize, timeElapsed / duration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        camera.orthographicSize = targetSize;
+    }
+
+    IEnumerator HandleCameraAndFadeIn(Camera camera, float targetSize, float duration)
+    {
+        yield return StartCoroutine(IncreaseCameraSize(camera, targetSize, duration));
+        RemainTime.SetActive(true);
+        yield return new WaitForSeconds(15f);
+        FadeIn.gameObject.SetActive(true);
+
+        Color fadeColor = FadeIn.color;
+        float fadeDuration = 2f; // ÌéòÏù¥Îìú Ïù∏ ÏãúÍ∞Ñ ÏÑ§Ï†ï
+        float timeElapsed = 0f;
+
+        while (timeElapsed < fadeDuration)
+        {
+            fadeColor.a = Mathf.Lerp(0, 1, timeElapsed / fadeDuration);
+            FadeIn.color = fadeColor;
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        fadeColor.a = 1;
+        FadeIn.color = fadeColor;
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("Main Stage");
     }
 }
