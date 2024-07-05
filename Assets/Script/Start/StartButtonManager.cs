6653811a7a5cad78a21529282d9112de35c07e4b
meokbo00 <meokbo00@naver.com>
2024-07-05 17:58:27 +0900

    123

--- Assets/Script/Start/StartButtonManager.cs
+++ Assets/Script/Start/StartButtonManager.cs
@@ -6,69 +6,56 @@ using UnityEngine.SceneManagement;
 
 public class StartButtonManager : MonoBehaviour
 {
-    public GameObject SinglePlaySetting;
-    public Button SinglePlayer;
-    public Button MultiPlayer;
-    public Button OnlinePlay;
+    public GameObject p1orp2;
+    public GameObject singleSetting;
+    public Button Play;
     public Button Setting;
     public Button Quit;
 
+    public Button singleP;
+    public Button multiP;
+    public Button x;
+
+
     public AudioSource ButtonAudio;
-
-    private TextMeshProUGUI singlePlayerText;
-    private TextMeshProUGUI onlinePlayText;
-
-    private Coroutine resetSinglePlayerCoroutine;
-    private Coroutine resetOnlinePlayCoroutine;
 
     void Start()
     {
-        singlePlayerText = SinglePlayer.GetComponentInChildren<TextMeshProUGUI>();
-        onlinePlayText = OnlinePlay.GetComponentInChildren<TextMeshProUGUI>();
+        this.Play.onClick.AddListener(() =>
+        {
+            ButtonAudio.Play();
+            p1orp2.gameObject.SetActive(true);
+        });
 
-        SinglePlayer.onClick.AddListener(OnSinglePlayerClicked);
-        MultiPlayer.onClick.AddListener(OnMultiPlayerClicked);
-        OnlinePlay.onClick.AddListener(OnOnlinePlayClicked);
-        Setting.onClick.AddListener(OnSettingClicked);
-        Quit.onClick.AddListener(OnQuitClicked);
-    }
+        this.Setting.onClick.AddListener(() =>
+        {
+            ButtonAudio.Play();
+            SceneManager.LoadScene("Setting Scene");
+        });
 
-    void OnSinglePlayerClicked()
-    {
-        ButtonAudio.Play();
-        SinglePlaySetting.gameObject.SetActive(true);
-    }
-    void OnMultiPlayerClicked()
-    {
-        ButtonAudio.Play();
-        SceneManager.LoadScene("Main Scene");
-    }
+        this.Quit.onClick.AddListener(() =>
+        {
+            Debug.Log("게임 종료!");
+            ButtonAudio.Play();
+            Application.Quit();
+        });
 
-    void OnOnlinePlayClicked()
-    {
-        ButtonAudio.Play();
-        if (resetOnlinePlayCoroutine != null)
-            StopCoroutine(resetOnlinePlayCoroutine);
+        this.singleP.onClick.AddListener(() =>
+        {
+            ButtonAudio.Play();
+            singleSetting.gameObject.SetActive(true);
+        });
 
-        onlinePlayText.text = "Coming Soon!";
-        resetOnlinePlayCoroutine = StartCoroutine(ResetTextAfterDelay(onlinePlayText, "Online Play", 3f));
-    }
+        this.multiP.onClick.AddListener(() =>
+        {
+            ButtonAudio.Play();
+            SceneManager.LoadScene("Main Scene");
+        });
 
-    void OnSettingClicked()
-    {
-        ButtonAudio.Play();
-        SceneManager.LoadScene("Setting Scene");
-    }
-
-    void OnQuitClicked()
-    {
-        ButtonAudio.Play();
-        Application.Quit();
-        Debug.Log("게임 종료!");
-    }
-    IEnumerator ResetTextAfterDelay(TextMeshProUGUI text, string originalText, float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        text.text = originalText;
+        this.x.onClick.AddListener(() =>
+        {
+            ButtonAudio.Play();
+            this.p1orp2.gameObject.SetActive(false);
+        });
     }
 }
