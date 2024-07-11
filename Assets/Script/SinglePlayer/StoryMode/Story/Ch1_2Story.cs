f384fd36e6dda6ae3a874c35b0428907f71dec1d
meokbo00 <meokbo00@naver.com>
2024-07-12 01:33:37 +0900

    123

--- Assets/Script/SinglePlayer/StoryMode/Story/Ch1_2Story.cs
+++ Assets/Script/SinglePlayer/StoryMode/Story/Ch1_2Story.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement; // �� ��ȯ�� ���� �߰�
 
 public class Ch1_2Story : MonoBehaviour
 {
     StageGameManager stageGameManager;
+    StageBallController stageBallController;
     private ShowText showText;
     public GameObject Clock;
     public GameObject Fadeinout;
@@ -19,6 +21,7 @@ public class Ch1_2Story : MonoBehaviour
         fadeImage = Fadeinout.GetComponent<Image>();
         stageGameManager = FindObjectOfType<StageGameManager>();
         TextManager textManager = FindObjectOfType<TextManager>();
+        stageBallController = FindObjectOfType<StageBallController>();
 
         Color color = fadeImage.color;
         color.a = 1f;
@@ -26,6 +29,7 @@ public class Ch1_2Story : MonoBehaviour
 
         if (stageGameManager.StageClearID == 6)
         {
+            stageBallController.enabled = false;
             Fadeinout.SetActive(true);
             textManager.GiveMeTextId(4);
             Clock.SetActive(true);
@@ -33,9 +37,8 @@ public class Ch1_2Story : MonoBehaviour
             remainTime.years = 0;
             remainTime.days = 0;
             remainTime.hours = 0;
-            remainTime.minutes = 2;
-            remainTime.seconds = 30;
-
+            remainTime.minutes = 0;
+            remainTime.seconds = 0;
 
             SpriteRenderer[] spriteRenderers = creation.GetComponentsInChildren<SpriteRenderer>();
             foreach (SpriteRenderer sr in spriteRenderers)
@@ -57,14 +60,36 @@ public class Ch1_2Story : MonoBehaviour
 
         if (showText != null && stageGameManager.StageClearID == 6)
         {
-            if (showText.logTextIndex == 2)
+            if (showText.logTextIndex > 38)
             {
+                stageBallController.enabled = true;
                 Fadeinout.SetActive(false);
             }
-            if (showText.logTextIndex == 8)
+            if (showText.logTextIndex == 48)
             {
-
+                StartCoroutine(FadeIn());
             }
         }
+    }
+
+    IEnumerator FadeIn()
+    {
+        Fadeinout.SetActive(true);
+        Color color = fadeImage.color;
+        color.a = 0f;
+        fadeImage.color = color;
+
+        while (color.a < 1f)
+        {
+            color.a += Time.deltaTime / 5; // 2�ʿ� ���� ���̵� ��
+            fadeImage.color = color;
+            yield return null;
+        }
+
+        // ���İ��� 1�� �Ǹ� 3�� ���
+        yield return new WaitForSeconds(3f);
+
+        // "Main Stage"�� �� ��ȯ
+        SceneManager.LoadScene("Main Stage");
     }
 }
--- Assets/Script/SinglePlayer/StoryMode/Story/Ch2Story.cs
+++ Assets/Script/SinglePlayer/StoryMode/Story/Ch2Story.cs
@@ -4,14 +4,83 @@ using UnityEngine;
 
 public class Ch2Story : MonoBehaviour
 {
+    public Camera Camera;
+    public GameObject Fadein;
+    StageGameManager stageGameManager;
+    StageBallController stageBallController;
+    TextManager textManager;
+    ShowText showText;
 
     void Start()
     {
+        showText = FindObjectOfType<ShowText>();
+        textManager = FindObjectOfType<TextManager>();
+        stageGameManager = FindObjectOfType<StageGameManager>();
+        stageBallController = FindObjectOfType<StageBallController>();
 
+
+        switch (stageGameManager.StageClearID)
+        {
+            case 6:
+                textManager.GiveMeTextId(1);
+                Fadein.SetActive(true);
+                Camera.orthographicSize = 150;
+
+                if (stageBallController != null)
+                {
+                    stageBallController.enabled = false;
+                }
+                break;
+            case 7:
+                textManager.GiveMeTextId(2);
+                break;
+            case 16:
+                textManager.GiveMeTextId(3);
+                break;
+            case 19:
+                textManager.GiveMeTextId(4);
+                break;
+            case 25:
+                textManager.GiveMeTextId(5);
+                break;
+            case 29:
+                textManager.GiveMeTextId(6);
+                break;
+            case 33:
+                textManager.GiveMeTextId(7);
+                break;
+        }
     }
 
     void Update()
     {
+        showText = FindObjectOfType<ShowText>();
+        if (showText != null && stageGameManager.StageClearID == 6)
+        {
+            if (showText.logTextIndex == 3)
+            {
+                Fadein.SetActive(false);
+            }
+            if (showText.logTextIndex == 4)
+            {
+                StartCoroutine(ChangeCameraSize(150, 4, 5f)); // 2�ʿ� ���� ī�޶� ������ ����
+            }
+            if (showText.logTextIndex == 36)
+            {
+                stageBallController.enabled = true;
+            }
+        }
+    }
 
+    IEnumerator ChangeCameraSize(float fromSize, float toSize, float duration)
+    {
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            Camera.orthographicSize = Mathf.Lerp(fromSize, toSize, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        Camera.orthographicSize = toSize;
     }
 }
