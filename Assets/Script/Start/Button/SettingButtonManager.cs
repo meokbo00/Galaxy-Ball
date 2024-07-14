00b46940d1d48a41b1bf6e1373b3582d4fb4b1ac
meokbo00 <meokbo00@naver.com>
2024-07-15 08:31:33 +0900

    123

--- Assets/Script/Start/Button/SettingButtonManager.cs
+++ Assets/Script/Start/Button/SettingButtonManager.cs
@@ -11,51 +11,71 @@ public class SettingButtonManager : MonoBehaviour
     public Button Sound_Effect_OFF;
     public Button Credit;
     public Button Back;
-
     public AudioSource ButtonAudio;
 
-    public bool isBGM = true;
-    public bool isSoundEffect = true;
+    private BGMControl bGMControl;
 
     private void Start()
     {
-        this.BGM_ON.onClick.AddListener(() =>
-        {
-            isBGM = true;
-            ButtonAudio.Play();
-            BGM_ON.gameObject.SetActive(false);
-            BGM_OFF.gameObject.SetActive(true);
+        bGMControl = FindObjectOfType<BGMControl>();
+        LoadButtonStates();
+
+        SetupButton(BGM_ON, BGM_OFF, () => {
+            bGMControl.BGMSwitch = false;
+            bGMControl.SaveAudioSettings(); // 상태 저장
         });
-        this.BGM_OFF.onClick.AddListener(() =>
-        {
-            isBGM = true;
-            ButtonAudio.Play();
-            BGM_OFF.gameObject.SetActive(false);
-            BGM_ON.gameObject.SetActive(true);
+        SetupButton(BGM_OFF, BGM_ON, () => {
+            bGMControl.BGMSwitch = true;
+            bGMControl.SaveAudioSettings(); // 상태 저장
         });
-        this.Sound_Effect_ON.onClick.AddListener(() =>
-        {
-            isSoundEffect = true;
-            ButtonAudio.Play();
-            Sound_Effect_ON.gameObject.SetActive(false);
-            Sound_Effect_OFF.gameObject.SetActive(true);
+        SetupButton(Sound_Effect_ON, Sound_Effect_OFF, () => {
+            bGMControl.SoundEffectSwitch = false;
+            bGMControl.SaveAudioSettings(); // 상태 저장
         });
-        this.Sound_Effect_OFF.onClick.AddListener(() =>
-        {
-            isSoundEffect = true;
-            ButtonAudio.Play();
-            Sound_Effect_OFF.gameObject.SetActive(false);
-            Sound_Effect_ON.gameObject.SetActive(true);
+        SetupButton(Sound_Effect_OFF, Sound_Effect_ON, () => {
+            bGMControl.SoundEffectSwitch = true;
+            bGMControl.SaveAudioSettings(); // 상태 저장
         });
-        this.Credit.onClick.AddListener(() =>
+
+        Credit.onClick.AddListener(() =>
         {
             ButtonAudio.Play();
             SceneManager.LoadScene("Credit Scene");
         });
-        this.Back.onClick.AddListener(() =>
+
+        Back.onClick.AddListener(() =>
         {
             ButtonAudio.Play();
             SceneManager.LoadScene("Start Scene");
         });
+    }
+
+    private void SetupButton(Button onButton, Button offButton, Action action)
+    {
+        onButton.onClick.AddListener(() =>
+        {
+            action();
+            ButtonAudio.Play();
+            onButton.gameObject.SetActive(false);
+            offButton.gameObject.SetActive(true);
+            SaveButtonStates();
+        });
+    }
+
+    private void SaveButtonStates()
+    {
+        PlayerPrefs.SetInt("BGM_ON", BGM_ON.gameObject.activeSelf ? 1 : 0);
+        PlayerPrefs.SetInt("BGM_OFF", BGM_OFF.gameObject.activeSelf ? 1 : 0);
+        PlayerPrefs.SetInt("Sound_Effect_ON", Sound_Effect_ON.gameObject.activeSelf ? 1 : 0);
+        PlayerPrefs.SetInt("Sound_Effect_OFF", Sound_Effect_OFF.gameObject.activeSelf ? 1 : 0);
+        PlayerPrefs.Save();
+    }
+
+    private void LoadButtonStates()
+    {
+        BGM_ON.gameObject.SetActive(PlayerPrefs.GetInt("BGM_ON", 1) == 1);
+        BGM_OFF.gameObject.SetActive(PlayerPrefs.GetInt("BGM_OFF", 0) == 1);
+        Sound_Effect_ON.gameObject.SetActive(PlayerPrefs.GetInt("Sound_Effect_ON", 1) == 1);
+        Sound_Effect_OFF.gameObject.SetActive(PlayerPrefs.GetInt("Sound_Effect_OFF", 0) == 1);
     }
 }
