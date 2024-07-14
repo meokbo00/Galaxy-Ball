66107e7c5051d6809068ecbc4f80d2d02065d6d1
meokbo00 <meokbo00@naver.com>
2024-07-15 08:32:37 +0900

    123

--- Assets/Script/Start/Button/SettingButtonManager.cs
+++ Assets/Script/Start/Button/SettingButtonManager.cs
@@ -13,7 +13,6 @@ public class SettingButtonManager : MonoBehaviour
     public Button Back;
     public AudioSource ButtonAudio;
 
-<<<<<<< HEAD
     private BGMControl bGMControl;
 
     private void Start()
@@ -36,39 +35,6 @@ public class SettingButtonManager : MonoBehaviour
         SetupButton(Sound_Effect_OFF, Sound_Effect_ON, () => {
             bGMControl.SoundEffectSwitch = true;
             bGMControl.SaveAudioSettings(); // 상태 저장
-=======
-
-    private void Start()
-    {
-        BGMControl bGMControl = FindObjectOfType<BGMControl>();
-        this.BGM_ON.onClick.AddListener(() =>
-        {
-            bGMControl.BGMSwitch = false;
-            ButtonAudio.Play();
-            BGM_ON.gameObject.SetActive(false);
-            BGM_OFF.gameObject.SetActive(true);
-        });
-        this.BGM_OFF.onClick.AddListener(() =>
-        {
-            bGMControl.BGMSwitch = true;
-            ButtonAudio.Play();
-            BGM_OFF.gameObject.SetActive(false);
-            BGM_ON.gameObject.SetActive(true);
-        });
-        this.Sound_Effect_ON.onClick.AddListener(() =>
-        {
-            bGMControl.SoundEffectSwitch = false;
-            ButtonAudio.Play();
-            Sound_Effect_ON.gameObject.SetActive(false);
-            Sound_Effect_OFF.gameObject.SetActive(true);
-        });
-        this.Sound_Effect_OFF.onClick.AddListener(() =>
-        {
-            bGMControl.SoundEffectSwitch = true;
-            ButtonAudio.Play();
-            Sound_Effect_OFF.gameObject.SetActive(false);
-            Sound_Effect_ON.gameObject.SetActive(true);
->>>>>>> 87dd06591c60f830e31410541e510eb7931d2614
         });
 
         Credit.onClick.AddListener(() =>
